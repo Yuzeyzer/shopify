10000c0ea77906ca134d54bd84d8df744592f3fe
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-03 05:43:34 +0600

    test

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
         autoplay:true,
         loop:true
     });
+    $('.mobile__btn').on('click', function(){
+        $(this).toggleClass('mobile__btn_active');
+        $('.menu').toggleClass('nav-list-active')
+    });
+    $('.nav__list_item').on('click', function(){
+        $('.nav-list').removeClass('nav-list-active');
+        $('.mobile__btn').removeClass('mobile__btn_active');
+    });
   });
